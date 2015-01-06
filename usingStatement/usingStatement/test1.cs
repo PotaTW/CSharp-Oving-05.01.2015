853597dd4c66f3ec55bad85f47cf83530ec94c41
Thomas Wiborg <loricariidae@hotmailcom>
2015-01-06 21:57:31 +0100

    namespace
    Learing about namespace, share it between projects

---
+++ usingStatement/usingStatement/test1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usingStatement
+{
+    public class test1
+    {
+        public int x = 5;
+    }
+}
